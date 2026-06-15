2e4f55496a4816d55aa63027450daf43727246c7
zhenggongze <406081648@qq.com>
2026-06-15 11:32:34 +0800

    Add pandas import for akshare

--- valuation/valuation_workflow.py
+++ valuation/valuation_workflow.py
@@ -12,6 +12,7 @@ import sys
 import time
 import random
 import argparse
+import pandas as pd
 from datetime import datetime, timezone, timedelta
 
 # ============================================================
